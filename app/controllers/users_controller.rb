4eb496a1d4fe714e0e162ceeb0cc8d025659ead7
Faisal Ahmed <faisalzone@msn.com>
2017-03-05 10:21:40 +0000

    Add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [:edit, :update] # For index, new, create and show, no need for any restrictions. The only time we need a restriction at the user's control level, is only while editing the user's profile.
+    before_action :require_same_user, only: [:edit, :update]
     
     def index
        @users = User.paginate(page: params[:page], per_page: 5) 
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to the alpha blog #{@user.username}"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end
